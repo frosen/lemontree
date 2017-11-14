9b65c472232c38ddcf4e700d6fd10ac75e747bd0
卢乐颜 <frosen_lu@126.com>
2017-11-14 21:40:58 +0800

    人物方向

--- p1/assets/Script/Hero.ts
+++ p1/assets/Script/Hero.ts
@@ -5,6 +5,34 @@
 const {ccclass, property} = cc._decorator;
 import {MyEvent, MyName} from './ConstValue'
 
+// 方向
+enum Direction {
+    D0, D45, 
+	D90, D135, 
+	D180, D225, 
+	D270, D315, 
+	Dnone,
+}
+
+enum BodyDirection {
+    left,
+    littleLeft,
+    mid,
+    littleRight,
+    right,
+}
+
+// x和y轴的不同比率下对应的方向
+const DIR_RATE = [
+    {r: -2.4142, e1: Direction.D270,   e2: Direction.D90},
+    {r: -0.4142, e1: Direction.D315,   e2: Direction.D135},
+    {r:  0.4142, e1: Direction.D0,     e2: Direction.D180},
+    {r:  2.4142, e1: Direction.D45,    e2: Direction.D225},
+    {r: Number.MAX_VALUE, e1: Direction.D90,    e2: Direction.D270},
+]
+const CUT45: number = 0.3535;
+const CUT90: number = 0.5;
+
 @ccclass
 export default class Hero extends cc.Component {
 
@@ -14,7 +42,18 @@ export default class Hero extends cc.Component {
     @property
     speedMax: number = 3;
 
+    @property(cc.SpriteFrame)
+    midFrame: cc.SpriteFrame = null;
+
+    @property(cc.SpriteFrame)
+    littleLeftFrame: cc.SpriteFrame = null;
+
+    @property(cc.SpriteFrame)
+    leftFrame: cc.SpriteFrame = null;
+
     curPos: cc.Vec2 = null;
+
+    curBodyDir: BodyDirection = BodyDirection.mid;
 
     onLoad() {
         this.initEvent();
@@ -24,7 +63,6 @@ export default class Hero extends cc.Component {
         cc.systemEvent.on(MyName(MyEvent.move), function(e) {
             let {xRate, yRate} = e.detail;
             this.curPos = new cc.Vec2(xRate * this.rangeNode.width, yRate * this.rangeNode.height);
-            cc.log(this.rangeNode.width, this.rangeNode.height, this.curPos);
         }, this);
 
         cc.systemEvent.on(MyName(MyEvent.endMove), function(e) {
@@ -41,12 +79,23 @@ export default class Hero extends cc.Component {
     }
 
     update() {
-        this.updatePosition();
+        let {disX, disY, r} = this.updatePosition();
+
+        // 获取移动方向
+        let dir: Direction = this.getDirection(disX, disY, r);
+
+        // 根据移动方向，更新身体朝向
+        this.updateBodyDirection(dir);
     }
 
-    updatePosition() {
-        if (!this.curPos) return;
-        if (this.curPos.equals(this.node.position)) return;
+    updatePosition(): {disX: number, disY: number, r: number} {
+        if (!this.curPos || this.curPos.equals(this.node.position)) {           
+            return {
+                disX: 0,
+                disY: 0,
+                r: 0
+            };
+        }
 
         let aimX: number = this.curPos.x;
         let aimY: number = this.curPos.y;
@@ -58,8 +107,8 @@ export default class Hero extends cc.Component {
         let disY: number = aimY - oriY;
 
         let r2 = disX * disX + disY * disY;
-        if (r2 > this.speedMax * this.speedMax) {
-            let r = Math.pow(r2, 0.5);
+        let r = Math.pow(r2, 0.5);
+        if (r > this.speedMax) {           
             aimX = disX * this.speedMax / r + oriX;
             aimY = disY * this.speedMax / r + oriY;
         }
@@ -79,6 +128,95 @@ export default class Hero extends cc.Component {
         
         // 设置新位置
         this.node.setPosition(aimX, aimY);
+
+        return {
+            disX: disX,
+            disY: disY,
+            r: r
+        }
+    }
+
+    // 缓冲区，为防止方向变化频繁而抖动
+    xBuffer: number = 0;
+    yBuffer: number = 0;
+
+    getDirection(dx: number, dy: number, r: number): Direction {
+        let curDirection: Direction;
+
+        if (dy == 0) {
+            if (dx > 0) curDirection = Direction.D0;
+            else if (dx < 0) curDirection = Direction.D180;
+        } else {
+            let rate = (dx + r * this.xBuffer) / (dy + r * this.yBuffer);   
+            if (rate <= DIR_RATE[3].r)
+                if (rate <= DIR_RATE[1].r)
+                    if (rate <= DIR_RATE[0].r) curDirection = dy > 0 ? DIR_RATE[0].e1 : DIR_RATE[0].e2;
+                    else curDirection = dy > 0 ? DIR_RATE[1].e1 : DIR_RATE[1].e2;
+                else
+                    if (rate <= DIR_RATE[2].r) curDirection = dy > 0 ? DIR_RATE[2].e1 : DIR_RATE[2].e2;
+                    else curDirection = dy > 0 ? DIR_RATE[3].e1 : DIR_RATE[3].e2;
+            else
+                curDirection = dy > 0 ? DIR_RATE[4].e1 : DIR_RATE[4].e2;
+        }
+        
+        this.setBuffer(curDirection);
+
+        return curDirection;
+    }
+
+    setBuffer(dir: Direction) {
+        switch (dir) {
+            case Direction.D0:    this.xBuffer = 0;      this.yBuffer = CUT90;  break;
+            case Direction.D45:   this.xBuffer = CUT45;  this.yBuffer = CUT45;  break;
+            case Direction.D90:   this.xBuffer = CUT90;  this.yBuffer = 0;      break;
+            case Direction.D135:  this.xBuffer = CUT45;  this.yBuffer = -CUT45; break;              
+            case Direction.D180:  this.xBuffer = 0;      this.yBuffer = -CUT90; break;
+            case Direction.D225:  this.xBuffer = -CUT45; this.yBuffer = -CUT45; break;
+            case Direction.D270:  this.xBuffer = -CUT90; this.yBuffer = 0;      break;
+            case Direction.D315:  this.xBuffer = -CUT45; this.yBuffer = CUT45;  break;
+            case Direction.Dnone: this.xBuffer = 0;      this.yBuffer = 0;      break;
+            default: break;
+        }
+    }
+
+    updateBodyDirection(dir: Direction) {
+        let nextBodyDir: BodyDirection = BodyDirection.mid;
+        switch (dir) {
+            case Direction.D0:
+            case Direction.D180:
+            case Direction.Dnone:
+                nextBodyDir = BodyDirection.mid;
+                break;
+
+            case Direction.D315:
+            case Direction.D225:
+                nextBodyDir = BodyDirection.littleLeft;
+                break;
+
+            case Direction.D270:
+                nextBodyDir = BodyDirection.left;
+                break;
+
+            case Direction.D45:
+            case Direction.D135:
+                nextBodyDir = BodyDirection.littleRight;
+                break;
+
+            case Direction.D90:
+                nextBodyDir = BodyDirection.right;
+                break;
+        }
+
+        if (this.curBodyDir == nextBodyDir) return;  
+        this.curBodyDir = nextBodyDir; 
+        let sp: cc.Sprite = this.node.getComponent(cc.Sprite);
+        switch(nextBodyDir) {
+            case BodyDirection.mid:         sp.spriteFrame = this.midFrame;        this.node.scaleX = 1;  break;
+            case BodyDirection.littleLeft:  sp.spriteFrame = this.littleLeftFrame; this.node.scaleX = 1;  break;
+            case BodyDirection.left:        sp.spriteFrame = this.leftFrame;       this.node.scaleX = 1;  break;
+            case BodyDirection.littleRight: sp.spriteFrame = this.littleLeftFrame; this.node.scaleX = -1; break;
+            case BodyDirection.right:       sp.spriteFrame = this.leftFrame;       this.node.scaleX = -1; break;
+        }
     }
 
     hurtAct: cc.Action = null;
