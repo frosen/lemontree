b0683628f6b9e13c9f7c6c5ba4e9e7fd42f6e853
卢乐颜 <frosen_lu@126.com>
2017-12-17 22:28:34 +0800

    测试瓦片地图

---
+++ p2/assets/Texture/Tiled/meta_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="meta_tiles" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="meta_tiles.png" width="67" height="34"/>
+</tileset>
