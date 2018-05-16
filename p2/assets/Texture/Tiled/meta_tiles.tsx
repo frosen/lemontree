642fd93aa5ff0099ec1ab0b35f0376e81e11f744
卢乐颜 <frosen_lu@126.com>
2018-05-16 21:41:50 +0800

    refine meta

--- p2/assets/Texture/Tiled/meta_tiles.tsx
+++ p2/assets/Texture/Tiled/meta_tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="meta_tiles" tilewidth="32" tileheight="32" tilecount="16" columns="8">
- <image source="meta_tiles.png" width="256" height="64"/>
+<tileset name="meta_tiles" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="meta_tiles.png" width="256" height="256"/>
  <tile id="0">
   <properties>
    <property name="collidable" type="int" value="1"/>
